422df558e5124940aabad033c5447d57043625c5
George Lee <keoki.lee@gmail.com>
2014-12-13 20:58:05 -1000

    Committing progress on rna_transcription.
    Only failing the assertion part. I think I can refactor this code quite a bit
    before submitting it. I should try to make it as Clojure-ish as I can.

--- rna-transcription/rna_transcription.clj
+++ rna-transcription/rna_transcription.clj
@@ -1,4 +1,9 @@
 (ns rna-transcription)
 
-(defn to-rna [rna]
-  rna)
+(defn to-rna [rna] 
+  (if (= rna "") rna
+    (cond
+      (= (first rna) \C) (str "G" (to-rna (subs rna 1)))
+      (= (first rna) \G) (str "C" (to-rna (subs rna 1)))
+      (= (first rna) \A) (str "U" (to-rna (subs rna 1)))
+      :else (str "A" (to-rna (subs rna 1))))))
